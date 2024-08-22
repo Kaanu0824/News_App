9de495d6ad1d2531cbe6b889539ae00029e78366
Kaanu0824 <Kaanu737@gmail.com>
2024-08-22 08:05:26 +0530

    Footer Add

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import './App.css';
+import Footer from './Components/Footer/footer';
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/category/:category" element={<Home/> } />
       </Routes>
+      <Footer/>
     </Router>
   );
 }
---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./footer.css"
+
+function Footer() {
+  return (
+
+    <span className='footer' > Copyright &#169; 2024 SK. All right received. </span>
+  )
+}
+
+export default Footer
